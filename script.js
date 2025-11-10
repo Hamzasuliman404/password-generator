88f2c77eba1f15f2a985c493ea241e5a55f23752
Hamzasuliman404 <hamza.ahmedsuliman@outlook.com>
2025-11-10 21:11:58 +0300

    Display area works fine
    The issue of area changing when it's empty vs filed is solved by using line-height and min-height

---
+++ script.js
@@ -0,0 +1,8 @@
+let passEl = document.querySelector("#password-one")
+let passsEl = document.querySelector("#password-two")
+
+
+function displayText(){
+    passEl.textContent = "Hello from another side"
+    passsEl.textContent = "Hello from another side"
+}
