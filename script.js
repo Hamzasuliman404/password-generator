0e0b667f4f9f19bf57ad624409095139db616245
Hamzasuliman404 <hamza.ahmedsuliman@outlook.com>
2025-11-10 21:53:31 +0300

    generate password function work fine
    - The basic function of generate password work
    - The issue of empty vs filled appear again and solved by vertical align property

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
 let passEl = document.querySelector("#password-one")
 let passsEl = document.querySelector("#password-two")
 
 
-function displayText(){
-    passEl.textContent = "Hello from another side"
-    passsEl.textContent = "Hello from another side"
+
+
+function generatePassword(){
+    let passwordone =""
+    let passwordtwo =""
+
+    for(let i = 0;i<15;i++){
+        let index = Math.floor(Math.random() * 91)
+        passwordone += characters[index]
+    }
+
+    for(let i = 14;i>=0;i--){
+        let index = Math.floor(Math.random() * 91)
+        passwordtwo += characters[index]
+    }
+    
+    passEl.textContent = passwordone
+    passsEl.textContent = passwordtwo
 }
+
+
+
