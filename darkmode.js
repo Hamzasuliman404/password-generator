8add72d5c37783e9c9a788f7ebf0755a78bd239c
Hamzasuliman404 <hamza.ahmedsuliman@outlook.com>
2025-11-11 20:28:03 +0300

    Dark and light mode done

---
+++ darkmode.js
@@ -0,0 +1,30 @@
+let darkmode = localStorage.getItem("darkmode") /* This to get current theme from local storage */
+
+const themeSwitch = document.getElementById("theme-switch")
+
+console.log("Heelo")
+
+
+if(darkmode === "active"){
+    enableDarkMode()
+}
+
+function checkDarkMode(){
+    darkmode = localStorage.getItem("darkmode")
+    if(darkmode !== "active"){
+        enableDarkMode()
+    }
+    else {
+        disableDarkMode()
+    }
+}
+
+function enableDarkMode(){
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode','active')
+}
+
+function disableDarkMode(){
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode',null)
+}
